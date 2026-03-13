127627cff33c858fe7005155287bc5018bb224cc
ytcwd3 <826247901@qq.com>
2026-03-13 15:10:04 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -697,13 +697,13 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute('data-repo', 'ytcwd3/ytcwd3.github.io');
     script.setAttribute('data-repo-id', 'R_kgDORh25sw');
     script.setAttribute('data-category', 'General');
-    script.setAttribute('data-category-id', '');
+    script.setAttribute('data-category-id', 'DIC_kwDORh25s84C4J7t');
     script.setAttribute('data-mapping', 'pathname');
     script.setAttribute('data-strict', '0');
     script.setAttribute('data-reactions-enabled', '1');
     script.setAttribute('data-emit-metadata', '0');
-    script.setAttribute('data-input-position', 'top');
-    script.setAttribute('data-theme', 'light');
+    script.setAttribute('data-input-position', 'bottom');
+    script.setAttribute('data-theme', 'preferred_color_scheme');
     script.setAttribute('data-lang', 'zh-CN');
     script.setAttribute('data-loading', 'lazy');
     script.crossOrigin = 'anonymous';
